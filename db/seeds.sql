0ffcb636acfd4adf1f46fbaeeaa0627f5f8405eb
Caitlin Schrader <c.n.schrader22@gmail.com>
2019-05-21 18:24:29 -0500

    beginning of orm structure

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('1/3 pounder' false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon cheeseburger' false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('hamburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('blackbean burger', true);
