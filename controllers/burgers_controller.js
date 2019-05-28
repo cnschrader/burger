26726e0a07fa5e3338241d777f82429a9fa3cebf
Caitlin Schrader <c.n.schrader22@gmail.com>
2019-05-27 20:48:47 -0500

    updated controllers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -14,3 +14,11 @@ router.get("/", function(req, res) {
     });
   });
   
+
+  router.post("/", function(req, res) {
+    burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+  
