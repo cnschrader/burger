9cc4ca80d79b076f747b0693f12e7e0e15428f91
Caitlin Schrader <c.n.schrader22@gmail.com>
2019-05-27 20:45:34 -0500

    updated controllers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,16 @@
 const express = require("express");
 
 const router = express.Router();
+
+const burger = require("models/burgers.js");
+
+router.get("/", function(req, res) {
+    cat.all(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+  
