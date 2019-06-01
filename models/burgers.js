5be44d3f58dccb4a5ced5a8f1722ba935f08d312
Caitlin Schrader <c.n.schrader22@gmail.com>
2019-05-31 19:55:46 -0500

    updated some parameter names in the orm and burgers.js

--- models/burgers.js
+++ models/burgers.js
@@ -1,25 +1,19 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-  selectAll: function(cb) {
-    console.log("inside the orm")
-    orm.selectAll("burgers", function(res) {
-        console.log("inside the all")
-      cb(res);
-    });
-  },
+    selectAll: function(tableName, cb) {
+        orm.selectAll(tableName, cb);
+        cb(res);
+      },
   // The variables cols and vals are arrays.
-  insertOne: function(cols, vals, cb) {
-    orm.insertOne("burgers", cols, vals, function(res) {
-      cb(res);
-    });
+  insertOne: function(name, cb) {
+    orm.insertOne("burgers", ["burger_name", "devoured"], [name, false], cb);
+    cb(res);
   },
-  updateOne: function(cols, vals, cb) {
-    orm.updateOne("burgers", cols, vals, function(res) {
-      cb(res);
-    });
-  }
+  updateOne: function(name, cb) {
+    orm.updateOne("burgers", ["burger_name", "devoured"], [name, false], cb);
+  },
 };
 
-// Export the database functions for the controller (catsController.js).
+// Export the database functions for the controller.
 module.exports = burger;
